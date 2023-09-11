1a3d5b893ad7c0060bde46e0edb00eb8c2abc804
Brianclikclak <brianclikclak@hotmail.com>
2023-09-11 11:22:26 +0200

    Library controller

--- api-rest-library-books-bidirectional/src/main/java/com/api/rest/library/controllers/LibraryController.java
+++ api-rest-library-books-bidirectional/src/main/java/com/api/rest/library/controllers/LibraryController.java
@@ -1,10 +1,17 @@
 package com.api.rest.library.controllers;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +29,23 @@ public class LibraryController {
     @Autowired
     private LibraryRepository libraryRepository;
 
+    @GetMapping
+    public ResponseEntity<Page<Library>> listLibrarys(Pageable pageable) {
+        return ResponseEntity.ok(libraryRepository.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Library> findLibraryById(@PathVariable int id) {
+        Optional<Library> OptionalLibrary = libraryRepository.findById(id);
+
+        if (!OptionalLibrary.isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        return ResponseEntity.ok(OptionalLibrary.get());
+
+    }
+
     @PostMapping
     public ResponseEntity<Library> savedLibrary(@Valid @RequestBody Library library) {
         Library savedLibrary = libraryRepository.save(library);
@@ -30,6 +54,32 @@ public class LibraryController {
                 .buildAndExpand(savedLibrary.getId())
                 .toUri();
         return ResponseEntity.created(location).body(savedLibrary);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Library> editLibrary(@PathVariable int id, @Valid @RequestBody Library library) {
+        Optional<Library> OptionalLibrary = libraryRepository.findById(id);
+
+        if (!OptionalLibrary.isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        library.setId(OptionalLibrary.get().getId());
+        libraryRepository.save(library);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Library> deleteLibrary(@PathVariable int id) {
+        Optional<Library> OptionalLibrary = libraryRepository.findById(id);
+
+        if (!OptionalLibrary.isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        libraryRepository.delete(OptionalLibrary.get());
+        return ResponseEntity.noContent().build();
     }
 
 }
